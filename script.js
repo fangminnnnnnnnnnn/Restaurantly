4f4d9b59b3667fb33a5935871916d47543eafdee
MIN <y3332244@gmail.com>
2024-04-22 06:55:25 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Function to log a message
+function logMessage(message) {
+    console.log(message);
+}
+
+// Function to calculate the square of a number
+function calculateSquare(number) {
+    return number * number;
+}
